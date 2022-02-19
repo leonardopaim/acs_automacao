d7575b8f8d2a42530d14d89803b4af8e581e4d48
Leonardo Henrique Paim <leohpaim@gmail.com>
2022-02-19 11:22:30 -0300

    fix: correção no nome do parâmetro

--- lib/02 - Domain/Interfaces/fornecedor_repository.dart
+++ lib/02 - Domain/Interfaces/fornecedor_repository.dart
@@ -1,8 +1,8 @@
 import '../Entities/_entities.dart';
 
 abstract class FornecedorRepository {
-  void cadastrar(Fornecedor cliente);
-  void atualizar(Fornecedor cliente);
+  void cadastrar(Fornecedor fornecedor);
+  void atualizar(Fornecedor fornecedor);
   void deletar(int id);
   Fornecedor buscarPorId(int id);
   List<Fornecedor> buscarTodos();
